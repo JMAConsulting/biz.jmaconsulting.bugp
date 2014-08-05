a11e27cd934f07ea017328b037cf1529d9b1063a
Edsel <edsel.lopez@jmaconsulting.biz>
2014-08-05 19:16:33 +0530

    CRM-14795 Initial commit

---
+++ CRM/Grant/Form/Task/PickProfile.php
@@ -0,0 +1,176 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.5                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2014                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2014
+ * $Id$
+ *
+ */
+
+/**
+ * This class provides the functionality for batch profile update
+ */
+class CRM_Grant_Form_Task_PickProfile extends CRM_Grant_Form_Task {
+
+  /**
+   * the title of the group
+   *
+   * @var string
+   */
+  protected $_title;
+
+  /**
+   * maximum contacts that should be allowed to update
+   *
+   */
+  protected $_maxGrants = 100;
+
+  /**
+   * maximum profile fields that will be displayed
+   *
+   */
+  protected $_maxFields = 9;
+
+  /**
+   * variable to store redirect path
+   *
+   */
+  protected $_userContext;
+
+  /**
+   * build all the data structures needed to build the form
+   *
+   * @return void
+   * @access public
+   */
+  function preProcess() {
+    /*
+     * initialize the task and row fields
+     */
+    parent::preProcess();
+
+    $session = CRM_Core_Session::singleton();
+    $this->_userContext = $session->readUserContext();
+
+    $validate = FALSE;
+    //validations
+    if (count($this->_grantIds) > $this->_maxGrants) {
+      CRM_Core_Session::setStatus(ts("The maximum number of grants you can select for Batch Update is %1. You have selected %2. Please select fewer grants from your search results and try again.", array(1 => $this->_maxGrants, 2 => count($this->_grantIds))), ts('Maximum Exceeded'), 'error');
+      $validate = TRUE;
+    }
+
+    // then redirect if error
+    if ($validate) {
+      CRM_Utils_System::redirect($this->_userContext);
+    }
+  }
+
+  /**
+   * Build the form
+   *
+   * @access public
+   *
+   * @return void
+   */
+  function buildQuickForm() {
+    CRM_Utils_System::setTitle(ts('Batch Profile Update for Grant'));
+
+    $grantTypes = CRM_Core_PseudoConstant::get('CRM_Grant_DAO_Grant', 'grant_type_id');
+    foreach ($this->_grantIds as $id) {
+      $grantTypeId = CRM_Core_DAO::getFieldValue('CRM_Grant_DAO_Grant', $id, 'grant_type_id');
+      $this->_grantTypes[] = $grantTypes[$grantTypeId];
+    }
+
+    //add Contact type profiles
+    $this->_grantTypes[] = 'Grant';
+    $this->_grantTypes[] = 'Contact';
+    $this->_grantTypes[] = 'Individual';
+
+    $profiles = CRM_Core_BAO_UFGroup::getProfiles($this->_grantTypes);
+
+    if (empty($profiles)) {
+      $types = implode(' ' . ts('or') . ' ', $this->_contactTypes);
+      CRM_Core_Session::setStatus(ts("The contact type selected for Batch Update does not have a corresponding profile. Please set up a profile for %1s and try again.", array(1 => $types)), ts('No Profile Available'), 'error');
+      CRM_Utils_System::redirect($this->_userContext);
+    }
+    $ufGroupElement = $this->add('select', 'uf_group_id', ts('Select Profile'), array('' => ts('- select profile -')) + $profiles, TRUE);
+
+    $this->addDefaultButtons(ts('Continue >>'));
+  }
+
+  /**
+   * Add local and global form rules
+   *
+   * @access protected
+   *
+   * @return void
+   */
+  function addRules() {
+    $this->addFormRule(array('CRM_Grant_Form_Task_PickProfile', 'formRule'));
+  }
+
+  /**
+   * global validation rules for the form
+   *
+   * @param array $fields posted values of the form
+   *
+   * @return array list of errors to be posted back to the form
+   * @static
+   * @access public
+   */
+  static function formRule($fields) {
+    if (CRM_Core_BAO_UFField::checkProfileType($fields['uf_group_id'])) {
+      $errorMsg['uf_group_id'] = "You cannot select mix profile for batch update.";
+    }
+
+    if (!empty($errorMsg)) {
+      //  return $errorMsg;
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * process the form after the input has been submitted and validated
+   *
+   * @access public
+   *
+   * @return void
+   */
+  public function postProcess() {
+    $params = $this->exportValues();
+
+    $this->set('ufGroupId', $params['uf_group_id']);
+
+    // also reset the batch page so it gets new values from the db
+    $this->controller->resetPage('Batch');
+  }
+  //end of function
+}
+
---
+++ mrg.php
@@ -0,0 +1,223 @@
+<?php
+
+require_once 'mrg.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ */
+function mrg_civicrm_config(&$config) {
+  _mrg_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ */
+function mrg_civicrm_xmlMenu(&$files) {
+  _mrg_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ */
+function mrg_civicrm_install() {
+  return _mrg_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ */
+function mrg_civicrm_uninstall() {
+  return _mrg_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ */
+function mrg_civicrm_enable() {
+  return _mrg_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ */
+function mrg_civicrm_disable() {
+  return _mrg_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ */
+function mrg_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _mrg_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ */
+function mrg_civicrm_managed(&$entities) {
+  return _mrg_civix_civicrm_managed($entities);
+}
+
+/**
+ * Function to get list of grant fields for profile
+ * For now we only allow custom grant fields to be in
+ * profile
+ *
+ * @param boolean $addExtraFields true if special fields needs to be added
+ *
+ * @return return the list of grant fields
+ * @static
+ * @access public
+ */
+function getGrantFields() {
+  $grantFields = CRM_Grant_DAO_Grant::export();
+  $grantFields = array_merge($grantFields, CRM_Core_OptionValue::getFields($mode = 'grant'));
+       
+  $grantFields = array_merge($grantFields, CRM_Financial_DAO_FinancialType::export());
+    
+  foreach ($grantFields as $key => $var) {
+    $fields[$key] = $var;
+  }
+
+  $fields = array_merge($fields, CRM_Core_BAO_CustomField::getFieldsForImport('Grant'));
+   
+  return $fields;
+}
+
+function mrg_civicrm_searchTasks($objectName, &$tasks) {
+  if ($objectName == 'grant') {
+    foreach ($tasks as $key => $value) {
+      if ($value['title'] == 'Update Grants') {
+        $tasks[$key]['title'] = 'Batch Update Grants via Profile';
+        $tasks[$key]['class'] = array( 
+          'CRM_Grant_Form_Task_PickProfile',
+          'CRM_Grant_Form_Task_Batch',
+        );
+      }
+    }
+  }
+}
+
+function mrg_civicrm_buildForm($formName, &$form) {
+  // Code to be done to avoid core editing
+  if ($formName == "CRM_UF_Form_Field" && CRM_Core_Permission::access('CiviGrant')) {
+    $grantFields = getProfileFields();
+    $fields['Grant'] = $grantFields;
+    // Add the grant fields to the form
+    $originalFields = $form->getVar('_fields');
+    $form->setVar('_fields', array_merge(exportableFields('Grant'), $originalFields));
+    $originalSelect = $form->getVar('_selectFields');
+
+    foreach ($fields as $key => $value) {
+      foreach ($value as $key1 => $value1) {
+        //CRM-2676, replacing the conflict for same custom field name from different custom group.
+        if ($customFieldId = CRM_Core_BAO_CustomField::getKeyID($key1)) {
+          $customGroupId = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_CustomField', $customFieldId, 'custom_group_id');
+          $customGroupName = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_CustomGroup', $customGroupId, 'title');
+          $mapperFields[$key][$key1] = $value1['title'] . ' :: ' . $customGroupName;
+          $selectFields[$key][$key1] = $value1['title'];
+        }
+        else {
+          $mapperFields[$key][$key1] = $value1['title'];
+          $selectFields[$key][$key1] = $value1['title'];
+        }
+        $hasLocationTypes[$key][$key1] = CRM_Utils_Array::value('hasLocationType', $value1);
+      }
+    }
+    if (!empty($selectFields['Grant'])) {
+      $form->setVar('_selectFields', array_merge($selectFields['Grant'], $originalSelect));
+    }
+    if(!empty($noSearchable)) {
+      $form->assign('noSearchable', $noSearchable);
+    }
+    $grantArray = array(
+      'text' => 'Grant',
+      'attr' => array('value' => 'Grant')
+    );
+
+    foreach ($form->_elements as $eleKey => $eleVal) {
+      foreach ($eleVal as $optionKey => $optionVal) {
+        if ($optionKey == '_options') {
+          $form->_elements[$eleKey]->_options[0]['Grant'] = 'Grant';
+          $form->_elements[$eleKey]->_options[1]['Grant'] = $mapperFields['Grant'];
+        }
+        if ($optionKey == '_elements') {
+          $form->_elements[$eleKey]->_elements[0]->_options[] = $grantArray;
+        } 
+        if ($optionKey == '_js') {
+          $form->_elements[$eleKey]->_js .= 'hs_field_name_Grant = '. json_encode($mapperFields['Grant']) . ';';
+        }
+      }
+    } 
+    if ($form->_defaultValues && array_key_exists('field_name', $form->_defaultValues) 
+      && $form->_defaultValues['field_name'][0] == 'Grant') {
+      $defaults['field_name'] = $form->_defaultValues['field_name'];
+      $form->setDefaults($defaults);
+    }
+  }
+}
+
+function &exportableFields() {
+  $grantFields = array(
+                       'grant_status' => array(
+                                               'title' => 'Grant Status',
+                                               'name' => 'grant_status',
+                                               'data_type' => CRM_Utils_Type::T_STRING,
+                                               ),
+                       'grant_type' => array(
+                                             'title' => 'Grant Type',
+                                             'name' => 'grant_type',
+                                             'data_type' => CRM_Utils_Type::T_STRING,
+                                             ),
+                       'grant_money_transfer_date' => array(
+                                                            'title' => 'Grant Money Transfer Date',
+                                                            'name' => 'grant_money_transfer_date',
+                                                            'data_type' => CRM_Utils_Type::T_DATE,
+                                                            ),
+                       'grant_amount_requested' => array(
+                                                         'title' => 'Grant Amount Requested',
+                                                         'name' => 'grant_amount_requested',
+                                                         'where' => 'civicrm_grant.amount_requested',
+                                                         'data_type' => CRM_Utils_Type::T_FLOAT,
+                                                         ),
+                       'grant_application_received_date' => array(
+                                                                  'title' => 'Grant Application Recieved Date',
+                                                                  'name' => 'grant_application_received_date',
+                                                                  'data_type' => CRM_Utils_Type::T_DATE,
+                                                                  ),
+                       );
+
+  $fields = CRM_Grant_DAO_Grant::export();
+  $grantNote = array('grant_note' => array('title' => ts('Grant Note'),
+                                           'name' => 'grant_note',
+                                           'data_type' => CRM_Utils_Type::T_TEXT,
+                                           ));
+  $fields = array_merge($fields, $grantFields, $grantNote,
+                        CRM_Core_BAO_CustomField::getFieldsForImport('Grant')
+                        );
+  return $fields;
+}
+
+function getProfileFields() {
+  $exportableFields = exportableFields('Grant');
+      
+  $skipFields = array('grant_id', 'grant_contact_id', 'grant_type', 'grant_note', 'grant_status' );
+  foreach ($skipFields as $field) {
+    if (isset($exportableFields[$field])) {
+      unset($exportableFields[$field]);
+    }
+  }
+      
+  return $exportableFields;
+}
