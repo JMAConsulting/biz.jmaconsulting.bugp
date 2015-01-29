86f2448e5c1756a95d39aa41ce6d67ff5630b98e
Edsel <edsel.lopez@jmaconsulting.biz>
2015-01-29 15:34:52 +0530

    MRG-6 Rearranged columsn for contact summary

--- bugp.php
+++ bugp.php
@@ -277,7 +277,7 @@ function calculateFiscalYearForDate($inputDate, $fyStart, $fyEnd) {
 }
 
 function bugp_civicrm_searchColumns($objectName, &$headers, &$rows, &$selector) {
-  if ($objectName == 'grant' ) {
+  if ($objectName == 'grant') {
     $remove = array(
       'grant_amount_total' => 'Requested',
       'grant_application_received_date' => 'Application Received',
